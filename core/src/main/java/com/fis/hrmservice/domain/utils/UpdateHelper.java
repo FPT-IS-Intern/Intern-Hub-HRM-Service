cdcc1bfce16ad505bf83a7dff98e66e840d443c4
HuynhDucPhu2502 <huynhducphu2502@gmail.com>
2026-02-26 10:21:18 +0700

    import utils

---
+++ core/src/main/java/com/fis/hrmservice/domain/utils/UpdateHelper.java
@@ -0,0 +1,20 @@
+package com.fis.hrmservice.domain.utils;
+
+import java.util.function.BiPredicate;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+public class UpdateHelper {
+    public static <T> boolean applyIfChanged(
+            T newValue, Supplier<T> getter, Consumer<T> setter, BiPredicate<T, T> equals) {
+        if (newValue == null) return false;
+
+        T current = getter.get();
+
+        if (!equals.test(current, newValue)) {
+            setter.accept(newValue);
+            return true;
+        }
+        return false;
+    }
+}
